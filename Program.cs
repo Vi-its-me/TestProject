b561a169a4b9b6d2091c98954a7a6079bc1bc258
Vi <vitalij.makarov_student@mail.ru>
2024-10-21 08:46:17 +0500

    v0.1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text;
+using System.IO;
+using OfficeOpenXml;
+
+namespace Database_project
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ConnectionForm());
+
+        }
+    }
+}
